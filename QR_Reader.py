9273ea7c1672d9ef4fdd270ed9af51da96947053
Markus Iversflaten <43810590+prinsWindy@users.noreply.github.com>
2020-02-11 14:37:34 +0100

    Needs testing

--- QR_Reader.py
+++ QR_Reader.py
@@ -49,6 +49,38 @@ def QR_Scanner(img, thresh_incr=0):
 
     return img
 
+def QR_Scanner_visualized(img):
+    """Scan QR codes from image. Returns position, orientation and image with marked QR codes"""
+
+    blur = cv2.bilateralFilter(src=img, d=5, sigmaColor=75, sigmaSpace=75)
+    grayscale = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)  # Make grayscale image for filtering and thresholding
+    normalized_img = cv2.normalize(grayscale, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+    data = decode(normalized_img)
+
+    # Thresholding for greater contrast:
+    #ret, threshBlur = cv2.threshold(grayscale, 50 + thresh_incr, 255, cv2.THRESH_BINARY)
+    sorted_data = sorted(data, key=lambda x: x[0])  # Sort the QR codes in ascending order
+
+    for QR_Code in sorted_data:  # Go through all QR codes
+        polygon = np.int32([QR_Code.polygon])  # Convert from int64 to int32, polylines only accepts int32
+        normalized_img = cv2.cvtColor(normalized_img, cv2.COLOR_GRAY2BGR)
+        cv2.polylines(normalized_img, polygon, True, color=(0, 0, 255), thickness=10)  # Draw lines around QR-codes
+
+        points = polygon[0]  # Extract corner points
+        x1 = points[0][0]
+        y1 = points[0][1]
+        x2 = points[3][0]
+        y2 = points[3][1]
+
+        x = [p[0] for p in points]
+        y = [p[1] for p in points]
+        position = (sum(x) / len(points), sum(y) / len(points))  # Calculate center of each QR code
+
+        # Draw circles in the middle of QR codes:
+        cv2.circle(normalized_img, center=(int(position[0]), int(position[1])), radius=10, color=(255, 0, 0), thickness=-1)
+
+    return normalized_img
+
 
 img = cv2.imread("QRKodeBord.jpg")
 img = QR_Scanner(img, 130)
