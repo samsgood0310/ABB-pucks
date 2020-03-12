69be3c9731a336a8f132a64e81cc8c5749eb3174
Mr Ole <38229452+oleollee@users.noreply.github.com>
2020-03-12 10:26:18 +0100

    Update find_correct_exposure.py

--- find_correct_exposure.py
+++ find_correct_exposure.py
@@ -1,11 +1,13 @@
 import cv2 as cv
 
-from QR_Reader import QR_Scanner
+import configparser
+import time
+
+"""from QR_Reader import QR_Scanner
 from pyueye_example_camera import Camera
 from pyueye import ueye
 from pyueye_example_utils import ImageData, ImageBuffer
 from numpy import median
-import time
 import ImageFunctions_CV
 
 
@@ -38,16 +40,15 @@ gain = ueye.INT(10)
 ueye.is_SetHardwareGain(cam.handle(), gain, ueye.IS_IGNORE_PARAMETER, ueye.IS_IGNORE_PARAMETER,
                         ueye.IS_IGNORE_PARAMETER)
 
-d = ueye.DOUBLE()
-
 # Disable auto exposure
 dblEnable = ueye.DOUBLE(0)
 dblDummy = ueye.DOUBLE(0)
 ueye.is_SetAutoParameter(cam.handle(), ueye.IS_SET_ENABLE_AUTO_SENSOR_GAIN_SHUTTER, dblEnable, dblDummy)
 
-# Increment and round number
+# Increment
 increment = 2
 
+# Loop from lowest possible exposure to highest possible exposure, incremented by 2 (ms)
 for exposure in range(exposure_low, exposure_high, increment):
     # Set new exposure
     newExposure = ueye.DOUBLE(exposure)
@@ -59,13 +60,11 @@ for exposure in range(exposure_low, exposure_high, increment):
     puck_list = QR_Scanner(img)
     print(puck_list)
 
-
     # Checking exposure
+    d = ueye.DOUBLE()
     retVal = ueye.is_Exposure(cam.handle(), ueye.IS_EXPOSURE_CMD_GET_EXPOSURE, d, 8)
     if retVal == ueye.IS_SUCCESS:
         print('Currently set exposure time %8.3f ms' % d)
-
-
 
     # Position returns as None if no QR-code is found
     if puck_list:
@@ -75,34 +74,23 @@ for exposure in range(exposure_low, exposure_high, increment):
     #cv.imshow('result', img)
     #cv.waitKey(0)
 
-# Update increment and brightness values
-increment = 2
-print(range_indicator)
-exposure_low = range_indicator[0]
-exposure_high = range_indicator[-1]
-print(range_indicator)
+#_exposure = str(median(exposure_values))"""
+_exposure = str(100000)
 
-for exposure in range(exposure_low, exposure_high, increment):
-    # Set new exposure
-    newExposure = ueye.DOUBLE(exposure)
-    ret = ueye.is_Exposure(cam.handle(), ueye.IS_EXPOSURE_CMD_SET_EXPOSURE, newExposure, ueye.sizeof(newExposure))
+start = time.time()
 
-    time.sleep(0.1)
 
-    img = ImageFunctions_CV.capture_image(cam, 500, 195)
-    puck_list = QR_Scanner(img)
+config = configparser.ConfigParser()
+config.read('exposure.ini')
+cfgfile = open('exposure.ini', 'w')
 
-    # Checking exposure
-    retVal = ueye.is_Exposure(cam.handle(), ueye.IS_EXPOSURE_CMD_GET_EXPOSURE, d, 8)
-    if retVal == ueye.IS_SUCCESS:
-        print('Currently set exposure time %8.3f ms' % d)
+# Updating the value for exposure
+config.set('EXPOSURE', 'exposure', _exposure)
 
-    # Position returns as None if no QR-code is found
-    if puck_list:
-        exposure_values.append(exposure)
+config.write(cfgfile)
+cfgfile.close()
 
-    # Show image to see result of exposure setpoint change
-    #cv.imshow('result', img)
-    #cv.waitKey(0)
+end = time.time()
+print(end - start)
 
-print(median(exposure_values))
+timing = 0.0005436041355133057  # average time divided by 1000
